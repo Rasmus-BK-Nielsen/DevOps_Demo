1975c9276a7c55bfb8eb90437d23a8d72fcf4089
Rasmus Bigum Kallehave Nielsen <129666566+Rasmus-BK-Nielsen@users.noreply.github.com>
2024-06-17 13:39:30 +0200

    Changes to home

--- devops_frontend/src/Pages/Home/Home.js
+++ devops_frontend/src/Pages/Home/Home.js
@@ -4,7 +4,9 @@ export default function Home() {
      return (
           <div>
                <h1>Welcome to this DevOps presentation</h1>
-               <p>Created by Rasmus B.K. Nielsen</p>
+               <p>Created by Rasmus B.K. Nielsen - 313614</p>
+               <p>This is a project created to demonstrate the tools learned through the DevOps <br />
+               course at VIA University College</p>
           </div>
      );
 }
