87577d0dadb977edef7254f721735aafdf83654c
Rasmus Bigum Kallehave Nielsen <129666566+Rasmus-BK-Nielsen@users.noreply.github.com>
2024-06-17 23:41:57 +0200

    Frontend dev (#5)
    * Added pages+index, also installed react-router-dom
    
    * Changes to css
    
    * Changes to home
    
    * Dockerfiles for production and deployment + scripts for sync with gitHub
    
    * Changes to Readme
    
    * Changes to accomodate nginx and private token
    
    * These Dockerfiles aint doing what I want
    
    * Filepaths where wrong, changes should now reflect a working Dockerfile
    
    * I am so tired of trying to fix docker.....
    
    * ....
    
    * more docker fixes
    
    * Back to the beginning
    
    * Maybe it works now?
    
    * A change to test for git errors
    
    * Git error gone maybe?
    
    * Final Docker maybe
    
    * Holy moly, let this be over please!
    
    * Test for removal of workdir
    
    * Testing and pipeline
    
    * Changes to the pipeline yml

--- devops_frontend/src/App.test.js
+++ devops_frontend/src/App.test.js
@@ -1,8 +1,56 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Home as option', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Question1 as option', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Question 1/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Question2 as option', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Question 2/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Question3 as option', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Question 3/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Question4 as option', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Question 4/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Question5 as option', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Question 5/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Question6 as option', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Question 6/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Feature as option', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Feature/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render ME!', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Rasmus B.K. Nielsen/i);
   expect(linkElement).toBeInTheDocument();
 });
