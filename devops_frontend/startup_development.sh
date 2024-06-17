b52fb080555fb51d526e9a8c41ca4be1181bfadb
Rasmus Bigum Kallehave Nielsen <129666566+Rasmus-BK-Nielsen@users.noreply.github.com>
2024-06-17 17:26:15 +0200

    more docker fixes

--- devops_frontend/startup_development.sh
+++ devops_frontend/startup_development.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Pull the latest changes from the repository
+# Navigate to the app directory
 cd /app/devops_frontend
+
+# Stash any local changes
 git stash
+
+# Pull the latest changes from the repository
 git pull origin frontend_dev
-git stash pop
+
+# Apply the stashed changes
+git stash pop || echo "No local changes to apply"
 
 # Install any new dependencies
 npm install
