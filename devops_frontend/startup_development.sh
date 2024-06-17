62ec4a8548401f87b86efba291a18ea1a7d89cf6
Rasmus Bigum Kallehave Nielsen <129666566+Rasmus-BK-Nielsen@users.noreply.github.com>
2024-06-17 17:33:58 +0200

    Back to the beginning

--- devops_frontend/startup_development.sh
+++ devops_frontend/startup_development.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Navigate to the app directory
-cd /app/devops_frontend
-
-# Stash any local changes
-git stash
-
 # Pull the latest changes from the repository
+cd /app/DevOps_Demo
 git pull origin frontend_dev
-
-# Apply the stashed changes
-git stash pop || echo "No local changes to apply"
 
 # Install any new dependencies
 npm install
